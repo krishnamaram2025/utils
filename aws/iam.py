9e69435c32e1d4c2975efbc5d3f7f4d92337358b
krishnamaram2025 <112494492+krishnamaram2025@users.noreply.github.com>
2023-02-10 13:25:07 +0530

    Update iam.py

--- aws/iam.py
+++ aws/iam.py
@@ -1,2 +1,19 @@
 import boto3
-iam_client = boto3.client('aim', 
+import os
+import sys
+from datetime import date
+
+accesskey = str(sys.argv[1])
+secretkey = str(sys.argv[2])
+username = 'admin'
+
+iam_client = boto3.client('iam', region_name = 'us-east-1', aws_access_key_id = accesskey, aws_secret_access_key = secretkey)
+keys = iam_client.list_access_keys(UserName=username)
+print("keys", keys)
+for key in keys['AccessKeyMetadta']:
+  if key['AccessKeyId'] == accesskey:
+    print("key", key)
+    accesskeybirthdate = key['CreateDate'].date()
+    todaydate = date.today()
+    age = todaydate - accesskeybirthdate
+    print("age", age)
