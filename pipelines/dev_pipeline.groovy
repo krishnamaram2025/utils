23273dab47535bab14d8cb39148c763049808e88
krishnamaram2025 <112494492+krishnamaram2025@users.noreply.github.com>
2023-02-10 14:05:54 +0530

    Update dev_pipeline.groovy

--- pipelines/dev_pipeline.groovy
+++ pipelines/dev_pipeline.groovy
@@ -1,11 +1,20 @@
 
+
+deployrepo = "https://github.com/krishnamaram2025/utils.git"
+deployreponame = "utils"
+Dir = "${deployreponame}/aws
 pipeline {
 
 agent any
+   
+environment {
+    AWS_ACCESS_KEY_ID = credentials('access_key_id')
+    AWS_SECRET_ACCESS_KEY = credentials('secret_key_id')
+    }
 
 parameters {
-    string(name: "accesskey", description: "accesskey")
-    string(name: "secretkey", description: "secretkey")
+    string(name: "Task", description: "task", defaultValue: "create")
+    string(name: "Branch_Name", description: "branch", defaultValue: "master")
 }
 
 stages {
@@ -19,11 +28,11 @@ steps{
 script {
 sh """
  whoami
- echo '${params.accesskey}' 
- echo '${params.secretkey}'
+ export AWS_ACCESS_KEY_ID = ${AWS_ACCESS_KEY_ID}
+ export AWS_SECRET_ACCESS_KEY = ${AWS_SECRET_ACCESS_KEY}
  sudo yum install python3-pip -y
  sudo pip3 install -r requirements.txt
- cd aws && sudo python3 iam.py '${params.accesskey}' '${params.secretkey}' 
+ cd ${Dir} && sudo python3 iam.py 
 """
 }
 }
