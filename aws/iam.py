caa10045c58f7d0b2e3d0b9cadab49e436d332a6
krishnamaram2025 <112494492+krishnamaram2025@users.noreply.github.com>
2023-02-10 14:17:29 +0530

    Update iam.py

--- aws/iam.py
+++ aws/iam.py
@@ -3,15 +3,15 @@ import os
 import sys
 from datetime import date
 
-accesskey = str(sys.argv[1])
-secretkey = str(sys.argv[2])
+# accesskey = str(sys.argv[1])
+# secretkey = str(sys.argv[2])
 username = 'admin'
 
-iam_client = boto3.client('iam', region_name = 'us-east-1', aws_access_key_id = accesskey, aws_secret_access_key = secretkey)
+iam_client = boto3.client('iam', region_name = 'us-east-1', aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY'])
 keys = iam_client.list_access_keys(UserName=username)
 print("keys", keys)
 for key in keys['AccessKeyMetadata']:
-  if key['AccessKeyId'] == accesskey:
+  if key['AccessKeyId'] == os.environ['AWS_ACCESS_KEY_ID']:
     print("key", key)
     accesskeybirthdate = key['CreateDate'].date()
     todaydate = date.today()
