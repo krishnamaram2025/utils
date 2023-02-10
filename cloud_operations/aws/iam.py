b64abf409588f6641894b7420bb8d244d6c42e47
krishnamaram2025 <112494492+krishnamaram2025@users.noreply.github.com>
2023-02-10 20:35:10 +0530

    Update iam.py

--- cloud_operations/aws/iam.py
+++ cloud_operations/aws/iam.py
@@ -2,14 +2,16 @@ import boto3
 import os
 import sys
 from datetime import date
+import time
 
 # accesskey = str(sys.argv[1])
 # secretkey = str(sys.argv[2])
 username = 'admin'
-# accesskey = os.environ['AWS_ACCESS_KEY_ID']
-# secretkey =  os.environ['AWS_SECRET_ACCESS_KEY']
-# print("accesskey", accesskey)
+accesskey = os.environ['AWS_ACCESS_KEY_ID']
+secretkey =  os.environ['AWS_SECRET_ACCESS_KEY']
+print("accesskey", accesskey)
 
+time.sleep(60)
 iam_client = boto3.client('iam', region_name = 'us-east-1')
 keys = iam_client.list_access_keys(UserName=username)
 print("keys", keys)
